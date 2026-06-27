2627da703866301761bb3409006025632ec77644
LiuHongWei <1453701384@qq.com>
2026-06-28 00:39:36 +0800

    005

---
+++ 005_triangle/log4cplus_log.h
@@ -0,0 +1,24 @@
+﻿/*- C --------------------------------------------------------------------------------------*\
+    Version     Author          Desciption
+    1.00        LiuHongWei      客户端日志工具, 对log4cplus的简单封闭, 具体参考cpp文件中test函数.
+    ...
+\*------------------------------------------------------------------------------------------*/
+
+#ifndef LHW_LOG_H
+#define LHW_LOG_H
+
+#include <log4cplus/logger.h>
+#include <log4cplus/loggingmacros.h>
+
+extern log4cplus::Logger daily_rolling_file_logger;
+
+void log4cplus_init();                              //log4cplus模块初始化
+
+#define LHW_FATAL(LOG_MSG) LOG4CPLUS_FATAL(daily_rolling_file_logger, LOG_MSG)
+#define LHW_ERROR(LOG_MSG) LOG4CPLUS_ERROR(daily_rolling_file_logger, LOG_MSG)
+#define LHW_WARN(LOG_MSG) LOG4CPLUS_WARN(daily_rolling_file_logger, LOG_MSG)
+#define LHW_INFO(LOG_MSG) LOG4CPLUS_INFO(daily_rolling_file_logger, LOG_MSG)
+#define LHW_DEBUG(LOG_MSG) LOG4CPLUS_DEBUG(daily_rolling_file_logger, LOG_MSG)
+#define LHW_TRACE(LOG_MSG) LOG4CPLUS_TRACE(daily_rolling_file_logger, LOG_MSG)
+
+#endif // LHW_LOG_H
